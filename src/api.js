72d97519f3b23102d17ae856fbdea2f73e8d52ea
Joseph Botros <jrbotros@gmail.com>
2017-07-06 11:58:40 -0400

    fix(api): pass appropriate arguments to load/save (#67)
    Also includes dist changes that weren't previously webpacked (will
    prevent this when #65 is addressed).

--- src/api.js
+++ src/api.js
@@ -1,9 +1,15 @@
+/* eslint no-unused-vars: "off" */
+
+/**
+ * This file represents an API stub to be overwritten by derived extensions.
+ */
+
 import testAnnotations from 'redux/testAnnotations.json';
 
-export function load() {
+export function load(contextKey) {
   return Promise.resolve(testAnnotations);
 }
 
-export function save() {
+export function save(updatedMappings, contextKey) {
   return Promise.resolve();
 }
